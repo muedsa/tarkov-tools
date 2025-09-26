d5dafbe4ceca266918968cc350d80bbf44721d1a
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-09-26 13:37:21 +0800

    feat: 首页链接全部打开新的窗口

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,22 +11,22 @@ export default function Home() {
           <p>Buiding...</p>
           <ol className="mt-2 list-inside list-decimal text-sm/6 text-left">
             <li className="tracking-[-.01em]">
-              <Link href="/pages/pvp/found-in-raid-items-page">
+              <Link href="/pages/pvp/found-in-raid-items-page" target="_blank">
                 战局内带出物品 PVP
               </Link>
             </li>
             <li className="tracking-[-.01em]">
-              <Link href="/page/pvp/mixed-items-tasks-page">
+              <Link href="/page/pvp/mixed-items-tasks-page" target="_blank">
                 其他需求物品任务 PVP
               </Link>
             </li>
             <li className="tracking-[-.01em]">
-              <Link href="/pages/pve/found-in-raid-items-page">
+              <Link href="/pages/pve/found-in-raid-items-page" target="_blank">
                 战局内带出物品 PVE
               </Link>
             </li>
             <li className="tracking-[-.01em]">
-              <Link href="/pages/pve/mixed-items-tasks-page">
+              <Link href="/pages/pve/mixed-items-tasks-page" target="_blank">
                 其他需求物品任务 PVE
               </Link>
             </li>
