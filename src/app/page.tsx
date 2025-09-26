61b3a1db35a6bd15f2597a40b1728577330f5e6f
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-09-26 13:13:40 +0800

    feat: 首页链接

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,26 @@ export default function Home() {
         <main className="flex flex-col row-start-2 items-center">
           <div className="text-5xl">Tarkov Tools</div>
           <p>Buiding...</p>
-          <ol className="list-inside list-decimal text-sm/6 text-left">
+          <ol className="mt-2 list-inside list-decimal text-sm/6 text-left">
             <li className="tracking-[-.01em]">
-              <Link href="/pages/found-in-raid-items-page">战局内带出物品</Link>
+              <Link href="/pages/pvp/found-in-raid-items-page">
+                战局内带出物品 PVP
+              </Link>
             </li>
-            <li className="mb-2 tracking-[-.01em]">
-              <Link href="/pages/mixed-items-tasks-page">其他需求物品任务</Link>
+            <li className="tracking-[-.01em]">
+              <Link href="/page/pvp/mixed-items-tasks-page">
+                其他需求物品任务 PVP
+              </Link>
+            </li>
+            <li className="tracking-[-.01em]">
+              <Link href="/pages/pve/found-in-raid-items-page">
+                战局内带出物品 PVE
+              </Link>
+            </li>
+            <li className="tracking-[-.01em]">
+              <Link href="/pages/pve/mixed-items-tasks-page">
+                其他需求物品任务 PVE
+              </Link>
             </li>
           </ol>
         </main>
