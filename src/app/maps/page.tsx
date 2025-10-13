0176ea8aa68be30d577b0966bd2f856c7951f172
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-10-13 12:07:40 +0800

    feat: 地图页的 metadata

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "塔科夫地图",
+};
+
 export default function MapsPage() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
