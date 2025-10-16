ca8eb8bfaccef1d3e3a71b249f9dbf779adb01b3
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-10-16 15:48:34 +0800

    feat: 如果任务目标没有子内容则不渲染子内容Box

--- src/app/tasks/[task]/_components/task-objective.tsx
+++ src/app/tasks/[task]/_components/task-objective.tsx
@@ -22,134 +22,150 @@ export default function TaskObjective({
           objective.__typename === "TaskObjectiveUseItem") &&
           ` - 0 / ${objective.count}`}
       </div>
-      <div className="p-2 bg-gray-950/30">
-        {objective.maps.length > 0 && (
-          <div>
-            <span className="text-white">地图:</span>{" "}
-            {objective.maps.map((m) => m.name).join("、")}
-          </div>
-        )}
-        {objective.__typename === "TaskObjectiveBuildItem" && (
-          <div>
-            <span className="text-white">构建物品</span>{" "}
-            {objective.count > 1 && `X${objective.count}`}
-          </div>
-        )}
-        {objective.__typename === "TaskObjectiveExperience" && (
-          <>
+      {(objective.__typename === "TaskObjectiveBuildItem" ||
+        objective.__typename === "TaskObjectiveExperience" ||
+        objective.__typename === "TaskObjectiveExtract" ||
+        objective.__typename === "TaskObjectiveItem" ||
+        objective.__typename === "TaskObjectiveMark" ||
+        objective.__typename === "TaskObjectiveQuestItem" ||
+        objective.__typename === "TaskObjectiveShoot" ||
+        objective.__typename === "TaskObjectiveSkill" ||
+        objective.__typename === "TaskObjectiveTaskStatus" ||
+        objective.__typename === "TaskObjectiveTraderLevel" ||
+        objective.__typename === "TaskObjectiveTraderStanding" ||
+        objective.__typename === "TaskObjectiveUseItem" ||
+        objective.maps.length > 0) && (
+        <div className="p-2 bg-gray-950/30">
+          {objective.maps.length > 0 && (
             <div>
-              <span className="text-white">部位:</span>{" "}
-              {objective.healthEffect.bodyParts.join("、")}
+              <span className="text-white">地图:</span>{" "}
+              {objective.maps.map((m) => m.name).join("、")}
             </div>
+          )}
+          {objective.__typename === "TaskObjectiveBuildItem" && (
             <div>
-              <span className="text-white">效果:</span>{" "}
-              {objective.healthEffect.effects.join("、")}
+              <span className="text-white">构建物品</span>{" "}
+              {objective.count > 1 && `X${objective.count}`}
             </div>
-            <div>
-              <span className="text-white">时间:</span>{" "}
-              {`${objective.healthEffect.time.compareMethod} ${objective.healthEffect.time.value}`}
-            </div>
-          </>
-        )}
-        {objective.__typename === "TaskObjectiveExtract" && (
-          <div className="text-white">
-            以幸存状态撤离
-            {objective.count > 1 && ` ${objective.count}次`}
-          </div>
-        )}
-        {objective.__typename === "TaskObjectiveItem" && (
-          <div>
+          )}
+          {objective.__typename === "TaskObjectiveExperience" && (
+            <>
+              <div>
+                <span className="text-white">部位:</span>{" "}
+                {objective.healthEffect.bodyParts.join("、")}
+              </div>
+              <div>
+                <span className="text-white">效果:</span>{" "}
+                {objective.healthEffect.effects.join("、")}
+              </div>
+              <div>
+                <span className="text-white">时间:</span>{" "}
+                {`${objective.healthEffect.time.compareMethod} ${objective.healthEffect.time.value}`}
+              </div>
+            </>
+          )}
+          {objective.__typename === "TaskObjectiveExtract" && (
             <div className="text-white">
-              {taskObjectiveItemTypeName[objective.type] ?? objective.type}
-              {objective.items.length > 1 && "(其中任何一个)"}:
+              以幸存状态撤离
+              {objective.count > 1 && ` ${objective.count}次`}
             </div>
-            <div className="flex flex-wrap">
-              {objective.items.map((item) => (
-                <TaskItemImage key={item.id} item={item} />
-              ))}
-            </div>
-          </div>
-        )}
-        {objective.__typename === "TaskObjectiveMark" && (
-          <div>
-            <div className="text-white">使用下面物品标记:</div>
-            <div>
-              <TaskItemImage item={objective.markerItem} />
-            </div>
-          </div>
-        )}
-        {objective.__typename === "TaskObjectiveQuestItem" && (
-          <TaskItemImage item={objective.questItem} />
-        )}
-        {objective.__typename === "TaskObjectiveShoot" && (
-          <div>
-            <span className="text-white">目标:</span>{" "}
-            {objective.targetNames.join("、")}
-          </div>
-        )}
-        {objective.__typename === "TaskObjectiveSkill" && (
-          <div>
-            <span className="text-white">{objective.skillLevel.name}:</span>
-            {` Lv.${objective.skillLevel.level}`}
-          </div>
-        )}
-        {objective.__typename === "TaskObjectiveTaskStatus" && (
-          <>
-            <div>
-              <span className="text-white">关联任务:</span>{" "}
-              <Link
-                href={`/tasks/${objective.task.normalizedName}`}
-                target="_blank"
-              >
-                {objective.task.name}
-              </Link>
-            </div>
-            <div>
-              <span className="text-white">任务状态:</span>{" "}
-              {objective.status.join("、")}
-            </div>
-          </>
-        )}
-        {objective.__typename === "TaskObjectiveTraderLevel" && (
-          <>
-            <div>
-              <span className="text-white">商人:</span> {objective.trader.name}
-            </div>
-            <div>
-              <span className="text-white">等级:</span> {objective.level}
-            </div>
-          </>
-        )}
-        {objective.__typename === "TaskObjectiveTraderStanding" && (
-          <>
-            <div>
-              <span className="text-white">商人:</span> {objective.trader.name}
-            </div>
-            <div>
-              <span className="text-white">信任度</span>{" "}
-              {objective.compareMethod} {objective.value}
-            </div>
-          </>
-        )}
-        {objective.__typename === "TaskObjectiveUseItem" && (
-          <>
+          )}
+          {objective.__typename === "TaskObjectiveItem" && (
             <div>
               <div className="text-white">
-                使用{objective.useAny.length > 1 && "(其中任何一个)"}:
+                {taskObjectiveItemTypeName[objective.type] ?? objective.type}
+                {objective.items.length > 1 && "(其中任何一个)"}:
               </div>
               <div className="flex flex-wrap">
-                {objective.useAny.map((item) => (
+                {objective.items.map((item) => (
                   <TaskItemImage key={item.id} item={item} />
                 ))}
               </div>
             </div>
+          )}
+          {objective.__typename === "TaskObjectiveMark" && (
             <div>
-              <span className="text-white">数量</span> {objective.compareMethod}{" "}
-              {objective.count}
+              <div className="text-white">使用下面物品标记:</div>
+              <div>
+                <TaskItemImage item={objective.markerItem} />
+              </div>
             </div>
-          </>
-        )}
-      </div>
+          )}
+          {objective.__typename === "TaskObjectiveQuestItem" && (
+            <TaskItemImage item={objective.questItem} />
+          )}
+          {objective.__typename === "TaskObjectiveShoot" && (
+            <div>
+              <span className="text-white">目标:</span>{" "}
+              {objective.targetNames.join("、")}
+            </div>
+          )}
+          {objective.__typename === "TaskObjectiveSkill" && (
+            <div>
+              <span className="text-white">{objective.skillLevel.name}:</span>
+              {` Lv.${objective.skillLevel.level}`}
+            </div>
+          )}
+          {objective.__typename === "TaskObjectiveTaskStatus" && (
+            <>
+              <div>
+                <span className="text-white">关联任务:</span>{" "}
+                <Link
+                  href={`/tasks/${objective.task.normalizedName}`}
+                  target="_blank"
+                >
+                  {objective.task.name}
+                </Link>
+              </div>
+              <div>
+                <span className="text-white">任务状态:</span>{" "}
+                {objective.status.join("、")}
+              </div>
+            </>
+          )}
+          {objective.__typename === "TaskObjectiveTraderLevel" && (
+            <>
+              <div>
+                <span className="text-white">商人:</span>{" "}
+                {objective.trader.name}
+              </div>
+              <div>
+                <span className="text-white">等级:</span> {objective.level}
+              </div>
+            </>
+          )}
+          {objective.__typename === "TaskObjectiveTraderStanding" && (
+            <>
+              <div>
+                <span className="text-white">商人:</span>{" "}
+                {objective.trader.name}
+              </div>
+              <div>
+                <span className="text-white">信任度</span>{" "}
+                {objective.compareMethod} {objective.value}
+              </div>
+            </>
+          )}
+          {objective.__typename === "TaskObjectiveUseItem" && (
+            <>
+              <div>
+                <div className="text-white">
+                  使用{objective.useAny.length > 1 && "(其中任何一个)"}:
+                </div>
+                <div className="flex flex-wrap">
+                  {objective.useAny.map((item) => (
+                    <TaskItemImage key={item.id} item={item} />
+                  ))}
+                </div>
+              </div>
+              <div>
+                <span className="text-white">数量</span>{" "}
+                {objective.compareMethod} {objective.count}
+              </div>
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 }
