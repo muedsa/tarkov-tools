a81f1f5f5bf4223b6c8f8c38d39859d332533789
MUEDSA <7676275+muedsa@users.noreply.github.com>
2026-01-05 13:36:02 +0800

    fix: 防止空指针

--- src/uitls/image-util.tsx
+++ src/uitls/image-util.tsx
@@ -1,5 +1,5 @@
 export function handleTarkovDevImageLink(link: string) {
-  if (link.startsWith("https://assets.tarkov.dev/")) {
+  if (link && link.startsWith("https://assets.tarkov.dev/")) {
     return link.replace("https://assets.tarkov.dev/", "/tarkov/images/");
   } else {
     return link;
